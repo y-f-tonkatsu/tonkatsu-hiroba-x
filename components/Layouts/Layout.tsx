a5713fdfca20a19cc48d49737c887d6ecc7ce081
Tsuneari Hayashi <ts-hayashi@automagi.jp>
2022-02-18 22:39:07 +0900

    Adobe Font 用の外部script を Script に移動

--- components/Layouts/Layout.tsx
+++ components/Layouts/Layout.tsx
@@ -4,6 +4,7 @@ import styles from "./Layout.module.scss";
 import {MainContent} from "./MainContent";
 import Head from "next/head";
 import favicon from "../../public/favicon.ico";
+import Script from 'next/script'
 
 /**
  * レイアウト定義
@@ -15,9 +16,11 @@ export const Layout = ({children}: { children: ReactElement[] }) => {
                 <title>とんかつ道場</title>
                 <meta name="description" content="Y.F.とんかつの絵などがある"/>
                 <link rel="icon" href={favicon.src}/>
-                <script dangerouslySetInnerHTML={{
-                    __html:
-                        `
+            </Head>
+
+            <Script dangerouslySetInnerHTML={{
+                __html:
+                    `
                           (function(d) {
                             var config = {
                               kitId: 'jer2ken',
@@ -27,8 +30,7 @@ export const Layout = ({children}: { children: ReactElement[] }) => {
                             h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
                           })(document);
                         `
-                }}/>
-            </Head>
+            }}/>
 
             <div className={styles.containerLayout}>
                 <NavBar/>
