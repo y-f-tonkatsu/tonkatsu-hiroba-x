4a46d980abe3349faff0b84c09d5c5ed9745077d
Tsuneari Hayashi <ts-hayashi@automagi.jp>
2022-10-30 16:58:36 +0900

    イントロのロゴを変更

--- pages/opening/TonImageList.ts
+++ pages/opening/TonImageList.ts
@@ -1,31 +1,31 @@
 export const TonImageList: { id: string, path: string }[] = [
     {
         id: "logo1",
-        path: "logo/th_separated/logo1.png",
+        path: "logo/th_separated/mover1.png",
     },
     {
         id: "logo2",
-        path: "logo/th_separated/logo2.png",
+        path: "logo/th_separated/mover2.png",
     },
     {
         id: "logo3",
-        path: "logo/th_separated/logo3.png",
+        path: "logo/th_separated/mover3.png",
     },
     {
         id: "logo4",
-        path: "logo/th_separated/logo4.png",
+        path: "logo/th_separated/mover4.png",
     },
     {
         id: "logo5",
-        path: "logo/th_separated/logo5.png",
+        path: "logo/th_separated/mover5.png",
     },
     {
         id: "logo6",
-        path: "logo/th_separated/logo6.png",
+        path: "logo/th_separated/mover6.png",
     },
     {
         id: "logo7",
-        path: "logo/th_separated/logo7.png",
+        path: "logo/th_separated/mover7.png",
     },
     {
         id: "philosopher_normal",
