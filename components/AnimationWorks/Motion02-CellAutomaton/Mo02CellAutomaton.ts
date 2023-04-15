a59a95c6f92e61c45f26c105c7e6226aeb1a0e08
Tsuneari Hayashi <ts-hayashi@automagi.jp>
2023-04-15 13:29:22 +0900

    Work 189 を修正

--- components/AnimationWorks/Motion02-CellAutomaton/Mo02CellAutomaton.ts
+++ components/AnimationWorks/Motion02-CellAutomaton/Mo02CellAutomaton.ts
@@ -94,7 +94,6 @@ export class Mo02CellAutomaton extends DisplayObject {
     override draw() {
         super.draw();
         const ctx = this.layer.context;
-        const [w, h] = [this._size.width, this._size.height];
 
         this._cell.forEach((raw, y) => {
             raw.forEach((dot, x) => {
